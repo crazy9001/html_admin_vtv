5ec82cefc47ad975c1c7218031537235aec17355
Toinn <ngoctoi.it@gmail.com>
2018-11-15 18:02:25 +0700

    update scroll

--- js/media.js
+++ js/media.js
@@ -1,12 +1,13 @@
 var Media = Media || {};
 Media.SecretKey = '8bthZZPmTkbRmmBqbcDP3VeIaj0PMPt8MAHA83RlPPzBB25YNhS3WhAryiuB7J4O';
 Media.ApiEndPoin = 'http://localhost:9000/api';
+Media.Storage = 'http://localhost:9000/storage';
 Media.User = 1;
 $.ajaxSetup({
     headers: {'X-Authorization': Media.SecretKey}
 });
 
-var media_wrapper = $("#__attachments-view-720"), media_sidebar = $('.media-sidebar')
+var media_wrapper = $("#__attachments-view-720"), media_sidebar = $('#media-sidebar')
 
 Media.getSizeImageFromUrl = function(url){
     var tmpImg = new Image();
@@ -27,19 +28,24 @@ MediaSystem = {
             url: Media.ApiEndPoin + '/media/gallery',
             type: "GET",
             data: {user: 1, action: e},
+            beforeSend: function() {
+                $('.media-toolbar .spinner').addClass('is-active')
+            },
+            complete: function() {
+                $('.media-toolbar .spinner').removeClass('is-active')
+            },
             success: function (data, textStatus, xhr) {
                 if(xhr.status == 200){
                     var html = '';
                     $.each(data.files.data, function(index, file) {
-                        var thumbnail = '', typeArray =  file.mime_type.split('/'),parseThumb = $.parseJSON(file.thumbnails);
+                        console.log(file);
+                        var thumbnail = '', typeArray =  file.mime_type.split('/'),
                             file_name = '';
                         if (typeArray[0] == 'image') {
-                            thumbnail = '<img src="' + parseThumb[140] + '" draggable="false" alt="' + file.name + '">' ;
                             file_name = '<div class="filename">' +
                                 '           <div>' + file.name + '</div>' +
                                 '       </div>';
                         }else if(typeArray[0] == 'video'){
-                            thumbnail = '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSi4ynKraQDOVJJVTtnKK_k6j5vLBwPzL-JtZ5ecSSDc_xGUehn">' ;
                             file_name = '<div class="filename">' +
                                 '           <div>' + file.name + '</div>' +
                                 '       </div>';
@@ -48,13 +54,13 @@ MediaSystem = {
                                 '       data-id="' + file.id + '" class="attachment save-ready" ' +
                                 '       data-uploaded="'+ file.created_at +'"' +
                                 '       data-size="' + file.size + '"' +
-                                '       data-path="' + file.path + '"' +
+                                '       data-path="' + Media.Storage + file.path + '"' +
                                 '       data-name="' + file.name + '"' +
-                                '       data-thumb="' + parseThumb[320] + '"' +
+                                '       data-thumb-medium="' + Media.Storage + file.thumbnails.medium + '"' +
                                 '       data-mime="' + typeArray[1] + '">' +
                                 '   <div class="attachment-preview js select-attachment type-image subtype-jpeg landscape">' +
                                 '       <div class="thumbnail">' +
-                                '           <div class="centered">' + thumbnail + '</div>' +
+                                '           <div class="centered"><img src="'+  Media.Storage + file.thumbnails.small +'"></div>' +
                                 '       </div>' + file_name +
                                 '   </div>' +
                                 '   <button type="button" class="button-link check" tabindex="-1">' +
@@ -87,7 +93,8 @@ MediaSystem = {
             $("#__attachments-view-720 li:not(this)").removeClass('details');
             $(this).toggleClass('details');
             $(this).find('button').css({'display': 'block'});
-            var html = '<div tabindex="0" data-id="184" class="attachment-details save-ready">' +
+            var html = '<div id="attachment-details">' +
+                '           <div tabindex="0" data-id="184" class="attachment-details save-ready">' +
                 '                                <h2>' +
                 '                                    Chi tiết đính kèm <span class="settings-save-status">' +
                 '                                        <span class="spinner"></span>' +
@@ -96,7 +103,7 @@ MediaSystem = {
                 '                                </h2>' +
                 '                                <div class="attachment-info">' +
                 '                                    <div class="thumbnail thumbnail-image">' +
-                '                                        <img src="'+ $(this).attr('data-thumb') +'"' +
+                '                                        <img src="'+ $(this).attr('data-thumb-medium') +'"' +
                 '                                             draggable="false" alt="">' +
                 '                                    </div>' +
                 '                                    <div class="details">' +
@@ -171,7 +178,9 @@ MediaSystem = {
                 '                                        </option>' +
                 '                                    </select>' +
                 '                                </label>' +
-                '                            </div>';
+                '                            </div>' +
+                '         </div>';
+            html += '<script>$("#attachment-details").slimScroll({height: "auto"})</script>';
             media_sidebar.html(html);
             $('button.media-button-insert').prop("disabled", false)
         });
@@ -180,4 +189,5 @@ MediaSystem = {
 $(document).ready(function () {
     window.MediaGallery = window.MediaGallery || {},
     MediaSystem.bindActionToElement()
+
 });
